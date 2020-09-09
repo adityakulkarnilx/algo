9bb8ccc944f9f35370ac9c051f36e4fa4346b579
Aditya Kulkarni <aditya.embeddeddsp@gmail.com>
2020-09-09 16:34:10 -0500

    :Q Initial commit WQ

---
+++ fibonacci.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main()
+{
+   int a,b,sum,number;
+
+   // Initialize
+   sum = 0;
+
+   // Print first 6 numbers of the fibonacci series
+   for( number=0; number< 6; number++ )
+   {
+      if( number==0 )
+      {
+         printf("\n0");
+      }
+      else if (number==1)
+      {
+         printf("\t1");
+         a=0;
+         b=1;
+      }
+      else
+      {
+         sum = a+b;
+         printf("\t%d",sum);
+         a=b;
+         b= sum;
+      }
+   }
+   printf("\n");
+   return 0;
+}
